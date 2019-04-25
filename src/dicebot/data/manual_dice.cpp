6da858e2865a58c6ec2eac536231e7ed245accbb
im foolish <532468158@qq.com>
2019-04-26 02:21:33 +0800

    style: formated with clang-format

--- src/dicebot/data/manual_dice.cpp
+++ src/dicebot/data/manual_dice.cpp
@@ -1,109 +1,100 @@
 #include "./manual_dice.h"
 
-#include "../dice_roller.h"
 #include "../../cqsdk/utils/vendor/cpp-base64/base64.h"
+#include "../dice_roller.h"
 
-#include <boost/archive/binary_oarchive.hpp>
 #include <boost/archive/binary_iarchive.hpp>
+#include <boost/archive/binary_oarchive.hpp>
 
-namespace dicebot::manual{
+namespace dicebot::manual {
 
     std::regex regex_filter_manual_dice_part("^(?:\\+)?(\\d+)?[dD](\\d+)");
 
-    manual_dice::manual_dice(){
-        i_sum_result = 0; 
-    }
+    manual_dice::manual_dice() { i_sum_result = 0; }
 
-    manual_dice::operator bool() const noexcept{
-        return this->status == roll::roll_status::FINISHED;
-    }
+    manual_dice::operator bool() const noexcept { return this->status == roll::roll_status::FINISHED; }
 
-    manual_dice::manual_dice(const std::string & source){
+    manual_dice::manual_dice(const std::string& source) {
         i_sum_result = 0;
         this->add(source);
     }
 
-    void manual_dice::roll(const std::string & source){
-        try{
+    void manual_dice::roll(const std::string& source) {
+        try {
             this->status = roll::roll_status::UNINITIALIZED;
             unsigned int target = std::stoi(source) - 1;
-            if(target >= this->size()){ 
+            if (target >= this->size()) {
                 this->status = roll::roll_status::FINISHED;
-                return; 
-            }
-            else if(target < 0) return;
+                return;
+            } else if (target < 0)
+                return;
             vec_mdice::iterator iter_list = this->begin() + target;
             int i_face_of_die = (*iter_list).first;
             roll::dice_roll dr;
             roll::roll_base(dr, 1, i_face_of_die);
-            if(dr){
+            if (dr) {
                 this->i_sum_result -= (*iter_list).second;
                 (*iter_list).second = dr.summary;
                 this->i_sum_result += dr.summary;
-            }
-            else{
+            } else {
                 this->status = dr.status;
                 return;
             }
             this->status = roll::roll_status::FINISHED;
-        }
-        catch (const std::invalid_argument& ia){
-            #ifdef _DEBUG
-                logger::log("manual_dice", ia.what());
-            #endif
+        } catch (const std::invalid_argument& ia) {
+#ifdef _DEBUG
+            logger::log("manual_dice", ia.what());
+#endif
             this->status = roll::roll_status::DICE_NOT_AVAILABLE;
         }
     }
 
-    void manual_dice::kill(const std::string & source){
+    void manual_dice::kill(const std::string& source) {
         this->status = roll::roll_status::UNINITIALIZED;
-        try{
-            unsigned int target = std::stoi(source) -1;
-            if(target >= this->size()){
+        try {
+            unsigned int target = std::stoi(source) - 1;
+            if (target >= this->size()) {
                 this->status = roll::roll_status::FINISHED;
                 return;
-            }
-            else if(target < 0) return;
+            } else if (target < 0)
+                return;
             vec_mdice::iterator iter_list = (this->begin()) + target;
             i_sum_result -= (*iter_list).second;
             this->erase((iter_list));
-            if(this->status == roll::roll_status::UNINITIALIZED) this->status = roll::roll_status::FINISHED;
-        }
-        catch (const std::invalid_argument& ia){
-            #ifdef _DEBUG
-                logger::log("manual_dice", ia.what());
-            #endif
+            if (this->status == roll::roll_status::UNINITIALIZED) this->status = roll::roll_status::FINISHED;
+        } catch (const std::invalid_argument& ia) {
+#ifdef _DEBUG
+            logger::log("manual_dice", ia.what());
+#endif
             this->status = roll::roll_status::DICE_NOT_AVAILABLE;
         }
     }
 
-    void manual_dice::add(const std::string & source){
+    void manual_dice::add(const std::string& source) {
         std::regex regex_manual_part("^(?:\\+)?(\\d+)?[dD](\\d+)");
-        try{
+        try {
             this->status = roll::roll_status::DICE_NOT_AVAILABLE;
             std::string str_source_copy(source);
             std::smatch smatch_single;
-            while(str_source_copy.size() > 0){
-
+            while (str_source_copy.size() > 0) {
                 std::regex_search(str_source_copy, smatch_single, regex_manual_part);
-                if(smatch_single.begin() == smatch_single.end()) return;
+                if (smatch_single.begin() == smatch_single.end()) return;
                 int i_dice = 1;
-                if(smatch_single[1].matched) i_dice = std::stoi(smatch_single[1].str());
+                if (smatch_single[1].matched) i_dice = std::stoi(smatch_single[1].str());
                 int i_face = std::stoi(smatch_single[2].str());
 
-                if(!CHECK_LIMITS((this->size() + i_dice),i_face)){
+                if (!CHECK_LIMITS((this->size() + i_dice), i_face)) {
                     this->status = roll::roll_status::TOO_MANY_DICE;
                     return;
                 }
 
-                for(int i_iter = 0; i_iter < i_dice; i_iter++){
+                for (int i_iter = 0; i_iter < i_dice; i_iter++) {
                     roll::dice_roll dr;
                     roll::roll_base(dr, 1, i_face);
-                    if(dr){
+                    if (dr) {
                         this->push_back(pair_mdice(i_face, dr.summary));
                         this->i_sum_result += dr.summary;
-                    }
-                    else{
+                    } else {
                         this->status = dr.status;
                         return;
                     }
@@ -111,43 +102,41 @@ namespace dicebot::manual{
                 str_source_copy.assign(smatch_single.suffix().str());
             }
             this->status = roll::roll_status::FINISHED;
-        }
-        catch (const std::invalid_argument& ia){
-            #ifdef _DEBUG
-                logger::log("manual_dice", ia.what());
-            #endif
+        } catch (const std::invalid_argument& ia) {
+#ifdef _DEBUG
+            logger::log("manual_dice", ia.what());
+#endif
             this->status = roll::roll_status::DICE_NOT_AVAILABLE;
         }
     }
 
-    void manual_dice::killall(){
+    void manual_dice::killall() {
         this->status = roll::roll_status::UNINITIALIZED;
-        try{
+        try {
             this->clear();
             this->i_sum_result = 0;
-            if(this->status == roll::roll_status::UNINITIALIZED) this->status = roll::roll_status::FINISHED;
-        }
-        catch(const std::invalid_argument& ia){
-            #ifdef _DEBUG
-                logger::log("manual_dice", ia.what());
-            #endif
+            if (this->status == roll::roll_status::UNINITIALIZED) this->status = roll::roll_status::FINISHED;
+        } catch (const std::invalid_argument& ia) {
+#ifdef _DEBUG
+            logger::log("manual_dice", ia.what());
+#endif
             this->status = roll::roll_status::DICE_NOT_AVAILABLE;
         }
     }
 
-    std::string manual_dice::encode() const{
+    std::string manual_dice::encode() const {
         ostrs strs(ostrs::ate);
         boost::archive::binary_oarchive oa(strs);
         oa << this->size();
         auto iter_list = this->cbegin();
-        for(; iter_list != this->cend(); iter_list++){
+        for (; iter_list != this->cend(); iter_list++) {
             oa << ((*iter_list).first);
             oa << ((*iter_list).second);
         }
-        return base64_encode((const unsigned char *)(strs.str().c_str()),strs.str().size());
+        return base64_encode((const unsigned char*)(strs.str().c_str()), strs.str().size());
     }
 
-    void manual_dice::decode(std::string & source){
+    void manual_dice::decode(std::string& source) {
         this->clear();
         std::string source_copy(source);
         source_copy = base64_decode(source_copy);
@@ -157,7 +146,7 @@ namespace dicebot::manual{
         this->i_sum_result = 0;
         int len = 0;
         ia >> len;
-        for(int i_iter = 0; i_iter < len; i_iter++){
+        for (int i_iter = 0; i_iter < len; i_iter++) {
             int first = 0;
             ia >> first;
             int second = 0;
@@ -167,21 +156,23 @@ namespace dicebot::manual{
         }
     }
 
-    std::string manual_dice::str(){
+    std::string manual_dice::str() {
         ostrs ostrs_result(ostrs::ate);
         int i_sum_result = 0;
         vec_mdice::iterator iter_list = this->begin();
 
         bool hasDice = iter_list != this->end();
-        for(; iter_list != this->end(); iter_list++){
-            if(iter_list != this->begin()){
+        for (; iter_list != this->end(); iter_list++) {
+            if (iter_list != this->begin()) {
                 ostrs_result << " + ";
             }
             ostrs_result << (*iter_list).second << "(" << (*iter_list).first << ")";
             i_sum_result += (*iter_list).second;
         }
-        if(!hasDice) ostrs_result << u8"没有骰子了";
-        else ostrs_result << " = " << i_sum_result;
+        if (!hasDice)
+            ostrs_result << u8"没有骰子了";
+        else
+            ostrs_result << " = " << i_sum_result;
         return ostrs_result.str();
     }
-}
+} // namespace dicebot::manual
--- src/tests/test.cpp
+++ src/tests/test.cpp
@@ -1,41 +1,37 @@
 #ifdef _WIN32
-    #ifdef _DEBUG
-        #pragma comment(lib,"gtestd.lib")
-    #else
-        #pragma comment(lib,"gtest.lib")
-    #endif
+#ifdef _DEBUG
+#pragma comment(lib, "gtestd.lib")
+#else
+#pragma comment(lib, "gtest.lib")
+#endif
 #endif
 
 #define NO_COOLQ
 
-#include <list>
-#include <cmath>
 #include <climits>
+#include <cmath>
+#include <list>
 
 #include "gtest/gtest.h"
 
 #include "dicebot/dicebot.h"
 
-#include "./roll_test.h"
-#include "./number_test.h"
 #include "./interpreter_test.h"
+#include "./number_test.h"
+#include "./roll_test.h"
 
-class Protocol_Level_test : public ::testing::Test{
+class Protocol_Level_test : public ::testing::Test {
 protected:
-    Protocol_Level_test(){
-        dicebot::initialize("./build/test_db/");
-    }
-    ~Protocol_Level_test(){
-        dicebot::salvage();
-    }
+    Protocol_Level_test() { dicebot::initialize("./build/test_db/"); }
+    ~Protocol_Level_test() { dicebot::salvage(); }
 };
 
-TEST_F(Protocol_Level_test,test1){
+TEST_F(Protocol_Level_test, test1) {
     dicebot::event_info ei(123456);
     ei.nickname = "dynilath";
     std::string output;
-    dicebot::message_pipeline(".r2d20+4",ei,output);
-    ASSERT_GT(output.size(),0);
+    dicebot::message_pipeline(".r2d20+4", ei, output);
+    ASSERT_GT(output.size(), (unsigned)0);
 }
 
 int main(int argc, char **argv) {
