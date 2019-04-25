34b1bcf3415e2d818038aeb172134dab7b0579c0
im foolish <532468158@qq.com>
2019-04-26 02:22:21 +0800

    feat(.roll): replace old roll method with new parser

--- src/dicebot/protocols/protocol_roll_dice.cpp
+++ src/dicebot/protocols/protocol_roll_dice.cpp
@@ -1,21 +1,22 @@
 #include "./protocol_roll_dice.h"
 
-#include "../utils/dice_utils.h"
+#include "../../cqsdk/utils/vendor/cpp-base64/base64.h"
 #include "../data/nick_manager.h"
 #include "../data/profile_manager.h"
 #include "../dice_spliter.h"
-#include "../../cqsdk/utils/vendor/cpp-base64/base64.h"
-
-//regular dice, with detailed info
+#include "../parser/dicenalyzer.h"
+#include "../parser/parser.h"
+#include "../utils/dice_utils.h"
+// regular dice, with detailed info
 
 using namespace dicebot;
 using namespace dicebot::protocol;
 
-protocol_roll_dice::protocol_roll_dice(){
+protocol_roll_dice::protocol_roll_dice() {
     this->is_stand_alone = false;
     this->filter_command = std::regex("^s(?:ource)? *", std::regex_constants::icase);
     this->identifier_regex = "r(?:oll)?";
-    this->identifier_list = {"roll","r"};
+    this->identifier_list = {"roll", "r"};
     this->help_message = base64_decode(
         "6aqw5a2Q5oyH5LukKC5yb2xs5oiWLnIpCuaMh+S7"
         "pC5yIDFkMjDvvJrpqrDkuIDkuKoxZDIwCuaMh+S7"
@@ -35,109 +36,92 @@ protocol_roll_dice::protocol_roll_dice(){
         "k+aenArmjIfku6QucnMgb27vvJrkuIrov7DmjIfk"
         "u6TnmoTovoPnn63lvaLlvI8K5oyH5LukLnJzIDNk"
         "Nu+8mumqsDNkNu+8jOaXoOinhnJzIG9mZuaViOae"
-        "nO+8jOS7jeeEtuaYvuekuuivpue7hue7k+aenA=="
-        );
+        "nO+8jOS7jeeEtuaYvuekuuivpue7hue7k+aenA==");
 }
 
-bool protocol_roll_dice::resolve_request(
-        std::string const & message,
-        event_info & event,
-        std::string & response){
+bool protocol_roll_dice::resolve_request(std::string const& message, event_info& event, std::string& response) {
     bool detailed_roll_message = false;
     std::smatch match_list_command_detail;
     std::regex_search(message, match_list_command_detail, this->filter_command);
-    
+
     std::string message_cp;
-    profile::profile_manager * pfm = profile::profile_manager::get_instance();
+    profile::profile_manager* pfm = profile::profile_manager::get_instance();
 
     profile::var_pair var;
-    
-    if(pfm->get_value<profile::sys_var_type,profile::var_pair>(
-        profile::sys_var_type::rs_on,var,event.user_id)){
-            if(var.second) detailed_roll_message = true;
-            else detailed_roll_message = false;
+    if (pfm->get_value(profile::sys_var_type::rs_on, var, event.user_id)) {
+        detailed_roll_message = var.second != 0;
     }
 
-    if(match_list_command_detail.size() > 0){
+    if (match_list_command_detail.begin() != match_list_command_detail.end()) {
         message_cp = match_list_command_detail.suffix().str();
-        if(message_cp == "on"){
+        if (message_cp == "on") {
             var.second = var.first;
-            if(pfm->set_value<profile::sys_var_type,profile::var_pair>(
-                profile::sys_var_type::rs_on,var,event.user_id)){
+            if (pfm->set_value(profile::sys_var_type::rs_on, var, event.user_id)) {
                 output_constructor oc(event.nickname);
                 oc.append_message(u8"启用骰子详细输出");
                 response = oc.str();
                 return true;
-            }
-            else return false;
-        }
-        else if(message_cp == "off"){
+            } else
+                return false;
+        } else if (message_cp == "off") {
             var.second = !var.first;
-            if(pfm->set_value<profile::sys_var_type,profile::var_pair>(
-                profile::sys_var_type::rs_on,var,event.user_id)){
+            if (pfm->set_value(profile::sys_var_type::rs_on, var, event.user_id)) {
                 output_constructor oc(event.nickname);
                 oc.append_message(u8"关闭骰子详细输出");
                 response = oc.str();
                 return true;
-            }
-            else return false;
-        }
-        else{
+            } else
+                return false;
+        } else {
             detailed_roll_message = true;
         }
-    }
-    else message_cp = message;
+    } else
+        message_cp = message;
 
     std::string str_roll_command;
     std::string str_roll_detail;
-    std::string str_result;
-    std::string str_message;
+    // std::string str_result;
 
-    binary_tree_split_dice(message_cp, str_roll_command, str_roll_detail, str_result, str_message);
+    auto map = pfm->get_profile(event.user_id)->get_map<std::string, std::string>();
+    diceparser::parser parser(*map);
+    auto pcomp = parser.parse(message_cp, diceparser::oper_constants::oper_default);
+    auto p_dice = diceparser::build_component_from_syntax(pcomp.get());
 
-    if(str_roll_command.size() > 0){
+    if (p_dice) {
+        std::string str_roll_command;
+        std::string str_roll_detail;
+        std::string str_result;
+        diceparser::component::str_container strs_command;
+
+        p_dice->print(strs_command);
+        for (const auto& str : strs_command) str_roll_command.append(str);
+
+        auto p_dicelet = std::dynamic_pointer_cast<diceparser::dicelet>(p_dice);
+
+        if (p_dicelet) {
+            diceparser::component::str_container strs_detail;
+            diceparser::dicelet::result_container results;
+            p_dicelet->roll_dicelet(results, strs_detail);
+            for (const auto& res : results) {
+                str_result.append(res.str());
+            }
+            if (detailed_roll_message)
+                for (const auto& str : strs_detail) str_roll_detail.append(str);
+        } else {
+            diceparser::component::str_container strs_detail;
+            number result = p_dice->roll_the_dice(strs_detail);
+            str_result.append(result.str());
+            if (detailed_roll_message)
+                for (const auto& str : strs_detail) str_roll_detail.append(str);
+        }
+
         output_constructor oc(event.nickname);
-        if(str_message.size()>0) oc.append_message(str_message);
-        if(detailed_roll_message) oc.append_roll(str_roll_command,str_roll_detail,str_result);
-        else oc.append_roll(str_roll_command,"",str_result);
+        if (parser.tail.size() > 0) oc.append_message(parser.tail);
+        if (detailed_roll_message)
+            oc.append_roll(str_roll_command, str_roll_detail, str_result);
+        else
+            oc.append_roll(str_roll_command, "", str_result);
         response = oc.str();
-        return true;
-    }
-    else{
-        std::string saved_roll;
-        if(message_cp.size() > 0){
-            std::string message_for;
-
-            size_t p;
-            if(utils::find_space(message_cp,p)){
-                if(p < message_cp.length()) message_for = message_cp.substr(p+1);
-                message_cp = message_cp.substr(0,p);
-            }
-
-            if(pfm->get_value<std::string,std::string>(
-                message_cp, saved_roll, event.user_id)){
-                binary_tree_split_dice(saved_roll, str_roll_command, str_roll_detail, str_result, str_message);
-
-                output_constructor oc(event.nickname);
-                if(message_for.size()>0) oc.append_message(message_for);
-                if(detailed_roll_message) oc.append_roll(str_roll_command,str_roll_detail,str_result);
-                else oc.append_roll(str_roll_command,"",str_result);
-                response = oc.str();
-                return true;
-            }
-        }
-        
-        if(pfm->get_value<profile::def_roll_type,std::string>(
-            profile::def_roll_type::def_roll,saved_roll,event.user_id)){
-            binary_tree_split_dice(saved_roll, str_roll_command, str_roll_detail, str_result, str_message);
-
-            output_constructor oc(event.nickname);
-            if(message_cp.size()>0) oc.append_message(message_cp);
-            if(detailed_roll_message) oc.append_roll(str_roll_command,str_roll_detail,str_result);
-            else oc.append_roll(str_roll_command,"",str_result);
-            response = oc.str();
-            return true;
-        }
     }
     return false;
 }
