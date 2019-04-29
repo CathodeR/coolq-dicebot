fa0218df302417e5463009c1f85366a026e9b9ed
dynilath <532468158@qq.com>
2019-04-29 19:51:48 +0800

    test: htest now has time usage

--- src/tests/htest.cpp
+++ src/tests/htest.cpp
@@ -2,25 +2,26 @@
 #include <string>
 #include "dicebot/dicebot.h"
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
     std::string in;
-    
+
     dicebot::event_info ei(123456);
     ei.nickname = "dynilath";
     dicebot::initialize("./build/test_db/");
 
     bool flag = true;
-    while(flag){
+    while (flag) {
         std::cout << "> ";
         char buf[1024];
-        std::cin.getline(buf,1024);
+        std::cin.getline(buf, 1024);
         in.assign(buf);
-        if(in.size() < 1) continue;
-        if(in == "quit" | in == "q") break; 
+        if (in.size() < 1) continue;
+        if (in == "quit" | in == "q") break;
         std::string out;
-        dicebot::message_pipeline(in,ei,out);
-        std::cout << out <<std::endl;
+        clock_t start = clock();
+        dicebot::message_pipeline(in, ei, out);
+        clock_t end = clock();
+        std::cout << " <" << end - start << "> " << out << std::endl;
     }
 
     dicebot::salvage();
