c33bc45b4dd82ce8c61399284cc8ccc5642f44b1
dynilath <532468158@qq.com>
2019-05-21 15:43:59 +0800

    test: improve protocol test

--- src/tests/protocol_test.cpp
+++ src/tests/protocol_test.cpp
@@ -21,139 +21,145 @@ class Protocol_Level_test : public ::testing::Test {
 protected:
     Protocol_Level_test() { dicebot::initialize("./build/test_db/"); }
     ~Protocol_Level_test() { dicebot::salvage(); }
+
+public:
+    void test_call(dicebot::event_info &ei, const std::string &source, const std::regex &reg_test) {
+        std::string output;
+        dicebot::try_fill_nickname(ei);
+        dicebot::message_pipeline(source, ei, output);
+        ASSERT_EQ(std::regex_match(output, reg_test), true);
+    }
+
+    void base_call(dicebot::event_info &ei, const std::string &source) {
+        std::string output;
+        dicebot::try_fill_nickname(ei);
+        dicebot::message_pipeline(source, ei, output);
+    }
 };
 
 TEST_F(Protocol_Level_test, roll_2d20plus4) {
     dicebot::event_info ei(123456, 10000, dicebot::event_type::group);
     ei.nickname = "dynilath";
-    std::string output;
 
-    std::string source = ".r2d20+4";
+    std::string source_1 = ".r2d20+4";
+    std::string source_2 = ".r 2d20+4";
+    std::string source_3 = ".roll2d20+4";
+    std::string source_4 = ".roll 2d20+4";
 
-    dicebot::try_fill_nickname(ei);
-    dicebot::message_pipeline(".ndice", ei, output);
-    std::regex result_reg_d(u8"^ \\* .* 的新名字是 dice$");
-    ASSERT_EQ(std::regex_match(output, result_reg_d), true);
+    this->base_call(ei, ".ndice");
 
-    dicebot::try_fill_nickname(ei);
-    dicebot::message_pipeline(".rson", ei, output);
-    std::regex result_reg_on(u8"^ \\* dice 启用骰子详细输出$");
-    ASSERT_EQ(std::regex_match(output, result_reg_on), true);
+    std::regex result_reg(u8"^ \\* dice 掷骰: 2d20\\+4 = \\[\\d{1,2} \\+ \\d{1,2}\\]\\+4 = \\d{1,2}$");
+    std::regex result_reg_s(u8"^ \\* dice 掷骰: 2d20\\+4 = \\d{1,2}$");
 
-    dicebot::try_fill_nickname(ei);
-    dicebot::message_pipeline(source, ei, output);
-    std::regex result_reg_1(u8"^ \\* dice 掷骰: 2d20\\+4 = \\[\\d{1,2} \\+ \\d{1,2}\\]\\+4 = \\d{1,2}$");
-    ASSERT_EQ(std::regex_match(output, result_reg_1), true);
+    std::regex roll_source_on(u8"^ \\* dice 启用骰子详细输出$");
+    this->test_call(ei, ".rson", roll_source_on);
+    this->test_call(ei, ".r s on", roll_source_on);
+    this->test_call(ei, ".r s on", roll_source_on);
+    this->test_call(ei, ".roll source on", roll_source_on);
+    this->test_call(ei, source_1, result_reg);
+    this->test_call(ei, source_2, result_reg);
+    this->test_call(ei, source_3, result_reg);
+    this->test_call(ei, source_4, result_reg);
 
-    dicebot::try_fill_nickname(ei);
-    dicebot::message_pipeline(".rs2d20+4", ei, output);
-    ASSERT_EQ(std::regex_match(output, result_reg_1), true);
-
-    dicebot::try_fill_nickname(ei);
-    dicebot::message_pipeline(".rsoff", ei, output);
-    std::regex result_reg_off(u8"^ \\* dice 关闭骰子详细输出$");
-    ASSERT_EQ(std::regex_match(output, result_reg_off), true);
-
-    dicebot::try_fill_nickname(ei);
-    dicebot::message_pipeline(source, ei, output);
-    std::regex result_reg_2(u8"^ \\* dice 掷骰: 2d20\\+4 = \\d{1,2}$");
-    ASSERT_EQ(std::regex_match(output, result_reg_2), true);
-
-    dicebot::try_fill_nickname(ei);
-    dicebot::message_pipeline(".rs2d20+4", ei, output);
-    ASSERT_EQ(std::regex_match(output, result_reg_1), true);
+    std::regex roll_source_off(u8"^ \\* dice 关闭骰子详细输出$");
+    this->test_call(ei, ".rsoff", roll_source_off);
+    this->test_call(ei, ".r s off", roll_source_off);
+    this->test_call(ei, ".r s off", roll_source_off);
+    this->test_call(ei, ".roll source off", roll_source_off);
+    this->test_call(ei, source_1, result_reg_s);
+    this->test_call(ei, source_2, result_reg_s);
+    this->test_call(ei, source_3, result_reg_s);
+    this->test_call(ei, source_4, result_reg_s);
+    this->test_call(ei, ".rs2d20+4", result_reg);
+    this->test_call(ei, ".r s 2d20+4", result_reg);
+    this->test_call(ei, ".roll s 2d20+4", result_reg);
+    this->test_call(ei, ".roll source 2d20+4", result_reg);
 }
 
 TEST_F(Protocol_Level_test, roll_6sharp4d6kl3) {
     dicebot::event_info ei(123456, 10000, dicebot::event_type::group);
     ei.nickname = "dynilath";
-    std::string output;
 
     std::string source = ".r6#4d6kl3";
 
-    dicebot::try_fill_nickname(ei);
-    dicebot::message_pipeline(".ndice", ei, output);
-    std::regex result_reg_d(u8"^ \\* .* 的新名字是 dice$");
-    ASSERT_EQ(std::regex_match(output, result_reg_d), true);
+    this->base_call(ei, ".ndice");
+    this->base_call(ei, ".rson");
 
-    dicebot::try_fill_nickname(ei);
-    dicebot::message_pipeline(".rson", ei, output);
-    std::regex result_reg_on(u8"^ \\* dice 启用骰子详细输出$");
-    ASSERT_EQ(std::regex_match(output, result_reg_on), true);
-
-    dicebot::try_fill_nickname(ei);
-    dicebot::message_pipeline(source, ei, output);
-    std::regex result_reg_1(
+    std::regex result_reg(
         u8"^ \\* dice 掷骰: \\{(4d6kl3, ){5}4d6kl3\\} = \\{(\\[\\d\\*? \\+ \\d\\*? \\+ \\d\\*? \\+ \\d\\*?\\],){5}(\\[\\d\\*? \\+ "
         u8"\\d\\*? \\+ \\d\\*? \\+ \\d\\*?\\])\\} = \\{(\\d{1,2}, ){5}\\d{1,2}\\}$");
-    ASSERT_EQ(std::regex_match(output, result_reg_1), true);
+    this->test_call(ei, source, result_reg);
 
-    dicebot::try_fill_nickname(ei);
-    dicebot::message_pipeline(".rsoff", ei, output);
-    std::regex result_reg_off(u8"^ \\* dice 关闭骰子详细输出$");
-    ASSERT_EQ(std::regex_match(output, result_reg_off), true);
-
-    dicebot::try_fill_nickname(ei);
-    dicebot::message_pipeline(source, ei, output);
-    std::regex result_reg_2(u8"^ \\* dice 掷骰: \\{(4d6kl3, ){5}4d6kl3\\} = \\{(\\d{1,2}, ){5}\\d{1,2}\\}$");
-    ASSERT_EQ(std::regex_match(output, result_reg_2), true);
-
-    dicebot::try_fill_nickname(ei);
-    dicebot::message_pipeline(".rs6#4d6kl3", ei, output);
-    ASSERT_EQ(std::regex_match(output, result_reg_1), true);
+    this->test_call(ei, ".rsoff", std::regex(u8"^ \\* dice 关闭骰子详细输出$"));
+    std::regex result_reg_s(u8"^ \\* dice 掷骰: \\{(4d6kl3, ){5}4d6kl3\\} = \\{(\\d{1,2}, ){5}\\d{1,2}\\}$");
+    this->test_call(ei, source, result_reg_s);
+    this->test_call(ei, ".rs6#4d6kl3", result_reg);
 }
 
 TEST_F(Protocol_Level_test, name_dice) {
     dicebot::event_info ei(123456, 10000, dicebot::event_type::group);
     ei.nickname = "dynilath";
-    std::string output;
 
     ei.group_id = 10001;
-    dicebot::try_fill_nickname(ei);
-    dicebot::message_pipeline(".ndice1", ei, output);
-    std::regex result_reg_1(u8"^ \\* .* 的新名字是 dice1$");
-    ASSERT_EQ(std::regex_match(output, result_reg_1), true);
+    this->test_call(ei, ".ndice1", std::regex(u8"^ \\* .* 的新名字是 dice1$"));
+    this->test_call(ei, ".namedice1", std::regex(u8"^ \\* dice1 的新名字是 dice1$"));
+    this->test_call(ei, ".n   dice1", std::regex(u8"^ \\* dice1 的新名字是 dice1$"));
+    this->test_call(ei, ".name   dice1", std::regex(u8"^ \\* dice1 的新名字是 dice1$"));
 
     ei.group_id = 10002;
-    dicebot::try_fill_nickname(ei);
-    dicebot::message_pipeline(".ndice2", ei, output);
-    std::regex result_reg_2(u8"^ \\* .* 的新名字是 dice2$");
-    ASSERT_EQ(std::regex_match(output, result_reg_2), true);
+    this->test_call(ei, ".ndice2", std::regex(u8"^ \\* .* 的新名字是 dice2$"));
+    this->test_call(ei, ".namedice2", std::regex(u8"^ \\* dice2 的新名字是 dice2$"));
+    this->test_call(ei, ".n   dice2", std::regex(u8"^ \\* dice2 的新名字是 dice2$"));
+    this->test_call(ei, ".name   dice2", std::regex(u8"^ \\* dice2 的新名字是 dice2$"));
 
-    ei.group_id = 10001;
-    dicebot::try_fill_nickname(ei);
-    dicebot::message_pipeline(".rs2d20+4", ei, output);
     std::regex result_reg_r1(u8"^ \\* dice1 掷骰: 2d20\\+4 = \\[\\d{1,2} \\+ \\d{1,2}\\]\\+4 = \\d{1,2}$");
-    ASSERT_EQ(std::regex_match(output, result_reg_r1), true);
-
-    ei.group_id = 10002;
-    dicebot::try_fill_nickname(ei);
-    dicebot::message_pipeline(".rs2d20+4", ei, output);
     std::regex result_reg_r2(u8"^ \\* dice2 掷骰: 2d20\\+4 = \\[\\d{1,2} \\+ \\d{1,2}\\]\\+4 = \\d{1,2}$");
-    ASSERT_EQ(std::regex_match(output, result_reg_r2), true);
 
     ei.group_id = 10001;
-    dicebot::try_fill_nickname(ei);
-    dicebot::message_pipeline(".ndice", ei, output);
-    std::regex result_reg_d1(u8"^ \\* dice1 的新名字是 dice$");
-    ASSERT_EQ(std::regex_match(output, result_reg_d1), true);
+    this->test_call(ei, ".rs2d20+4", result_reg_r1);
 
     ei.group_id = 10002;
-    dicebot::try_fill_nickname(ei);
-    dicebot::message_pipeline(".ndice", ei, output);
-    std::regex result_reg_d2(u8"^ \\* dice2 的新名字是 dice$");
-    ASSERT_EQ(std::regex_match(output, result_reg_d2), true);
+    this->test_call(ei, ".rs2d20+4", result_reg_r2);
+
+    ei.group_id = 10001;
+    this->test_call(ei, ".ndice", std::regex(u8"^ \\* dice1 的新名字是 dice$"));
+
+    ei.group_id = 10002;
+    this->test_call(ei, ".ndice", std::regex(u8"^ \\* dice2 的新名字是 dice$"));
 }
 
 TEST_F(Protocol_Level_test, macro_recall) {
     dicebot::event_info ei(123456, 10000, dicebot::event_type::group);
     ei.nickname = "dynilath";
-    std::string output;
 
-    dicebot::try_fill_nickname(ei);
-    dicebot::message_pipeline(".rs2d20+4", ei, output);
-    std::regex result_reg_r2(u8"^ \\* dice2 掷骰: 2d20\\+4 = \\[\\d{1,2} \\+ \\d{1,2}\\]\\+4 = \\d{1,2}$");
-    ASSERT_EQ(std::regex_match(output, result_reg_r2), true);
+    this->base_call(ei, ".ndice");
+    this->test_call(ei, ".s 4d6", std::regex(u8"^ \\* dice 设置默认骰子指令: \\(4d6\\)"));
+    this->test_call(ei, ".set 4d6", std::regex(u8"^ \\* dice 设置默认骰子指令: \\(4d6\\)"));
+
+    std::regex result_reg(u8"^ \\* dice 掷骰: \\(4d6\\) = \\(\\[\\d \\+ \\d \\+ \\d \\+ \\d\\]\\) = \\d{1,2}$");
+    std::regex result_reg_s(u8"^ \\* dice 掷骰: \\(4d6\\) = \\d{1,2}$");
+
+    this->base_call(ei, ".rsoff");
+    this->test_call(ei, ".rs", result_reg);
+    this->test_call(ei, ".r", result_reg_s);
+
+    this->test_call(ei, ".s 4d6 test", std::regex(u8"^ \\* dice 设置指令: \\(4d6\\) 为 test"));
+    this->test_call(ei, ".set 4d6 test", std::regex(u8"^ \\* dice 设置指令: \\(4d6\\) 为 test"));
+
+    this->base_call(ei, ".rsoff");
+    this->test_call(ei, ".rs test", result_reg);
+    this->test_call(ei, ".r test", result_reg_s);
+
+    this->test_call(ei, ".s 4 test", std::regex(u8"^ \\* dice 设置指令: 4 为 test"));
+
+    std::regex result2_reg(u8"^ \\* dice 掷骰: 4d6 = \\[\\d \\+ \\d \\+ \\d \\+ \\d\\] = \\d{1,2}$");
+    std::regex result2_reg_s(u8"^ \\* dice 掷骰: 4d6 = \\d{1,2}$");
+
+    this->base_call(ei, ".rsoff");
+    this->test_call(ei, ".rs testd6", result2_reg);
+    this->test_call(ei, ".rs test d 6 ", result2_reg);
+    this->test_call(ei, ".r testd6", result2_reg_s);
+    this->test_call(ei, ".r test d 6", result2_reg_s);
 }
 
 int main(int argc, char **argv) {
