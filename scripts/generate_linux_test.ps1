138fc8273fc8f3576847ffb3f589efe8cf39dfef
dynilath <532468158@qq.com>
2019-05-24 13:55:48 +0800

    build: env not set
    it seems that in this version of powershell, not env could be imported
    from linux os

--- scripts/generate_linux_test.ps1
+++ scripts/generate_linux_test.ps1
@@ -1,15 +1,21 @@
 $config_type = 'Debug'
 
 Set-Location $PSScriptRoot\..\  # enter the parent folder
-if(-not (Test-Path -Path "build\$config_type")){
+if (-not (Test-Path -Path "build\$config_type")) {
     New-Item -Path "build\$config_type" -ItemType "directory"
 }
-if(-not (Test-Path -Path "build\test_db")){
+if (-not (Test-Path -Path "build\test_db")) {
     New-Item -Path "build\test_db" -ItemType "directory"
 }
 Set-Location .\build\$config_type  # enter the build folder
 
+if (-not (Test-Path -Path "$env:VCPKG_ROOT")) {
+    Write-Host "vcpkg_root not found" -ForegroundColor Red
+    Exit 1
+}
 $vcpkg_root = $env:VCPKG_ROOT
+Write-Host "vcpkg_root at $env:VCPKG_ROOT" -ForegroundColor Green
+
 $vcpkg_triplet = "x64-linux"
 
 cmake -DCMAKE_TOOLCHAIN_FILE="$vcpkg_root\scripts\buildsystems\vcpkg.cmake" `
